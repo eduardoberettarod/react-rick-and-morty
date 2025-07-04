fdbca41db240513c91523f373d172ef520e9814c
eduardoberettarod <eduardo.berettarod@gmail.com>
2025-07-03 22:18:06 -0300

    xcxc

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,12 @@ function App(){
 
   return (
     <>
-    <h1>Ola mundo</h1>
-
+    <div id="rick-sanchez">
+      <span></span>
+      <h1>Rick Sanchez</h1>
+      <p className='tags'>ENGENHEIRO</p>
+    <img src= "https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+    </div>
     </>
   )
 }
